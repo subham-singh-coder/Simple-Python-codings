71525ea00a9ac7ad29f224cf9a6251fcc7f06d65
subham-singh-coder <139640294+subham-singh-coder@users.noreply.github.com>
2023-07-15 21:09:53 +0530

    Pizza Ordering System
    The program would take user input for their pizza sizes.
    Based on their pizza size selection, the program would specify certain amount for the pizza. Once confirmed, The program would ask user is they wanted to add pepperoni over their pizza or not. Based on this selection, additional charges would be applies. Lastly, the program would ask weather user wanted to add extra cheese over their pizza or not, based on which certain additional amounting would be done. After this the final price for the pizza would be displayed with Thank you message

---
+++ Pizza Ordering system.py
@@ -0,0 +1,52 @@
+bill1=0
+print("Welcome to Pizza House!")
+size= str(input("Enter Your Pizza Size [s for Small, m for Medium, l for Large]:\n"))
+if size=='s' or size=='S':
+    bill1=bill1+15
+    print(f"The total bill for your {size} size Pizza is ${bill1}")
+    pep=str(input("Do you need Pepperoni over your Pizza? [Yes/No]\n"))
+    if pep=='Y' or pep=='y':
+        bill1=bill1+2
+        print(f"The total bill for your {size} size Pizza is ${bill1}")
+    else:
+        print(f"The total bill for your {size} size Pizza is ${bill1}")
+    che=str(input("DO you need extra cheese over your Pizza? [Yes/No]\n"))
+    if che=='y' or che=='Y':
+        bill1=bill1+1
+        print(f"The total bill for your {size} size Pizza is ${bill1}")
+    else:
+        print(f"The total bill for your {size} size Pizza is ${bill1}")
+    print("Thank You for Visiting Pizza House!")
+elif size=='m' or size=='M':
+    bill1 = bill1 + 20
+    print(f"The total bill for your {size} size Pizza is ${bill1}")
+    pep=str(input("Do you need Pepperoni over your Pizza? [Yes/No]\n"))
+    if pep=='Y' or pep=='y':
+        bill1=bill1+3
+        print(f"The total bill for your {size} size Pizza is ${bill1}")
+    else:
+        print(f"The total bill for your {size} size Pizza is ${bill1}")
+        che=str(input("DO you need extra cheese over your Pizza? [Yes/No]\n"))
+        if che=='y' or che=='Y':
+            bill1=bill1+1
+            print(f"The total bill for your {size} size Pizza is ${bill1}")
+        else:
+            print(f"The total bill for your {size} size Pizza is ${bill1}")
+    print("Thank You for Visiting Pizza House!")
+elif size=='l' or size=='L':
+    bill1 = bill1 + 25
+    print(f"The total bill for your {size} size Pizza is ${bill1}")
+    pep = str(input("Do you need Pepperoni over your Pizza? [Yes/No]\n"))
+    if pep == 'Y' or pep == 'y':
+        bill1 = bill1 + 3
+        print(f"The total bill for your {size} size Pizza is ${bill1}")
+    else:
+        print(f"The total bill for your {size} size Pizza is ${bill1}")
+        che = str(input("DO you need extra cheese over your Pizza? [Yes/No]\n"))
+        if che == 'y' or che == 'Y':
+            bill1 = bill1 + 1
+            print(f"The total bill for your {size} size Pizza is ${bill1}")
+        else:
+            print(f"The total bill for your {size} size Pizza is ${bill1}")
+    print("Thank You for Visiting Pizza House!")
+print("Visit Again!!")
